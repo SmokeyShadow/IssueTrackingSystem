8bcb0d24e4424f6030353dda3802d056d71cbcb7
DESKTOP-9LVHFSC\Atiyeh <atiehnorouzi22@gmail.com>
2019-04-24 21:51:42 +0430

    -fix bugs

--- pages/reporting/reportController.js
+++ pages/reporting/reportController.js
@@ -8,20 +8,143 @@
     function reportController($rootScope, $http) {
 
         $rootScope.bodylayout = 'main_page_que';
+        createGanttChart();
 
     }
-    app.controller('dashboardCtrl', function ($scope, $window ,myService) {
+    app.controller('dashboardCtrl', function ($scope, $window, myService) {
         var user = myService.get();
-        $scope.name = user.user ;
-        if(user.role.trim() != 'admin')
-        {
+        $scope.name = user.user;
+        if (user.role != 'admin') {
             $scope.adminAccess = 'hidden';
         }
-   
+
 
     });
 })();
+function createGanttChart() {
+    
+    // Themes begin
+    am4core.useTheme(am4themes_animated);
+    // Themes end
 
+    var chart = am4core.create("chartdiv", am4charts.XYChart);
+    chart.rtl = true;
+    chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
+
+    chart.paddingRight = 30;
+    chart.dateFormatter.inputDateFormat = "yyyy-MM-dd HH:mm";
+
+    var colorSet = new am4core.ColorSet();
+    colorSet.saturation = 0.4;
+
+    chart.data = [{
+        "category": "سیستم گرمایشی",
+        "start": "2016-01-01",
+        "end": "2016-01-14",
+        "color": colorSet.getIndex(0).brighten(0),
+        "task": " در حال بررسی توسط عطیه نوروززاده"
+    }, {
+        "category": "سیستم گرمایشی",
+        "start": "2016-01-16",
+        "end": "2016-01-27",
+        "color": colorSet.getIndex(0).brighten(0.4),
+        "task": " در حال بررسی توسط عطیه نوروززاده"
+    }, {
+        "category": "سیستم گرمایشی",
+        "start": "2016-02-05",
+        "end": "2016-04-18",
+        "color": colorSet.getIndex(0).brighten(0.8),
+        "task": " در حال انجام توسط عطیه نوروززاده"
+    }, {
+        "category": "سیستم سرمایشی",
+        "start": "2016-01-08",
+        "end": "2016-01-10",
+        "color": colorSet.getIndex(2).brighten(0),
+        "task": " در حال بررسی توسط عطیه نوروززاده"
+    }, {
+        "category": "سیستم سرمایشی",
+        "start": "2016-01-12",
+        "end": "2016-01-15",
+        "color": colorSet.getIndex(2).brighten(0.4),
+        "task": " در حال بررسی توسط عطیه نوروززاده"
+    }, {
+        "category": "سیستم سرمایشی",
+        "start": "2016-01-16",
+        "end": "2016-02-05",
+        "color": colorSet.getIndex(2).brighten(0.8),
+        "task": " در حال بررسی توسط عطیه نوروززاده"
+    }, {
+        "category": "سیستم سرمایشی",
+        "start": "2016-02-10",
+        "end": "2016-02-18",
+        "color": colorSet.getIndex(2).brighten(1.2),
+        "task": " در حال انجام توسط عطیه نوروززاده"
+    }, {
+        "category": "کلاس خالی",
+        "start": "2016-01-02",
+        "end": "2016-01-08",
+        "color": colorSet.getIndex(4).brighten(0),
+        "task": " در حال بررسی توسط عطیه نوروززاده"
+    }, {
+        "category": "کلاس خالی",
+        "start": "2016-01-19",
+        "end": "2016-03-01",
+        "color": colorSet.getIndex(4).brighten(0.8),
+        "task": " در حال بررسی توسط عطیه نوروززاده"
+    }, {
+        "category": "کلاس خالی",
+        "start": "2016-03-12",
+        "end": "2016-04-05",
+        "color": colorSet.getIndex(4).brighten(1.2),
+        "task": " در حال انجام توسط عطیه نوروززاده"
+    },  {
+        "category": "زمان بندی کلاس ها",
+        "start": "2016-04-27",
+        "end": "2016-05-15",
+        "color": colorSet.getIndex(6).brighten(1.2),
+        "task": " در حال انجام توسط عطیه نوروززاده"
+    }, {
+        "category": "ویدیو آموزشی",
+        "start": "2016-01-19",
+        "end": "2016-03-01",
+        "color": colorSet.getIndex(8).brighten(0.8),
+        "task": " در حال بررسی توسط عطیه نوروززاده"
+    }, {
+        "category": "ویدیو آموزشی",
+        "start": "2016-03-08",
+        "end": "2016-03-30",
+        "color": colorSet.getIndex(8).brighten(1.2),
+        "task": "در حال انجام توسط عطیه نوروززاده"
+    }];
+
+    chart.dateFormatter.dateFormat = "yyyy-MM-dd";
+    chart.dateFormatter.inputDateFormat = "yyyy-MM-dd";
+   
+    var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.dataFields.category = "category";
+    categoryAxis.renderer.grid.template.location = 0;
+    categoryAxis.renderer.inversed = false;
+    categoryAxis.rtl = true;
+    categoryAxis.parseDates = false;
+
+    var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+    dateAxis.renderer.minGridDistance = 70;
+    dateAxis.baseInterval = { count: 1, timeUnit: "day" };
+    dateAxis.renderer.tooltipLocation = 0;
+
+    var series1 = chart.series.push(new am4charts.ColumnSeries());
+    series1.columns.template.width = am4core.percent(80);
+    series1.columns.template.tooltipText = "{task}: [bold]{openDateX}[/] - [bold]{dateX}[/]";
+
+    series1.dataFields.openDateX = "start";
+    series1.dataFields.dateX = "end";
+    series1.dataFields.categoryY = "category";
+    series1.columns.template.propertyFields.fill = "color"; // get color from data
+    series1.columns.template.propertyFields.stroke = "color";
+    series1.columns.template.strokeOpacity = 1;
+
+    chart.scrollbarX = new am4core.Scrollbar();
+}
 function getOptions(sel) {
     var opts = [], opt;
     for (var i = 0, len = sel.options.length; i < len; i++) {
@@ -97,3 +220,4 @@ function filterByOne(id, filteredcell) {
         }
     }
 }
+
