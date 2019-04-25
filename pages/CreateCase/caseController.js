e65395c142830b84aa9b397469bfcb8f45f7afd7
DESKTOP-9LVHFSC\Atiyeh <atiehnorouzi22@gmail.com>
2019-04-26 03:16:47 +0430

    -change inputs for case page

--- pages/CreateCase/caseController.js
+++ pages/CreateCase/caseController.js
@@ -5,57 +5,26 @@
 
     caseController.$inject = ['$rootScope'];
 
-    function caseController($rootScope, $http ) {
+    function caseController($rootScope, $http) {
 
         $rootScope.bodylayout = 'main_page_que';
-
+        getDate();
     }
-    
-    app.controller('dashboardCtrl', function ($scope, $window ,myService) {
-		var user = myService.get();
-        $scope.name = user.user ;
-        if(user.role.trim() != 'admin')
-        {
+
+    app.controller('dashboardCtrl', function ($scope, $window, myService) {
+        var user = myService.get();
+        $scope.name = user.user;
+        if (user.role.trim() != 'admin') {
             $scope.adminAccess = 'hidden';
         }
-   
+
 
     });
 
 })();
 
-function chooseFunc(id){
-    var list = document.getElementById("list-one");
-    var dropdown = list.getElementsByClassName("dropdown");
-    var button = dropdown[0].getElementsByClassName("btn btn-primary dropdown-toggle");
-    if(id == "complaint"){
-        button[0].textContent = "شکایت";
-    }
-    else if(id == "critic"){
-        button[0].textContent = "انتقاد";
-    }
-    else if(id == "suggestion"){
-
-        button[0].textContent = "پیشنهاد";
-    }
-    else if(id == "purpose"){
-        button[0].textContent = "درخواست";
-    }
-
-}
-function chooseFunc2(id){
-    var list = document.getElementById("list-two");
-    var dropdown = list.getElementsByClassName("dropdown");
-    var button = dropdown[0].getElementsByClassName("btn btn-primary dropdown-toggle");
-    if(id == "common"){
-        button[0].textContent = "معمولی";
-    }
-    else if(id == "normal"){
-        button[0].textContent = "متوسط";
-    }
-    else if(id == "urgent"){
-
-        button[0].textContent = "اورژانسی";
-    }
+function getDate() {
+    var today = new Date();
+    document.getElementById("today").value = today.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear();
 
 }
